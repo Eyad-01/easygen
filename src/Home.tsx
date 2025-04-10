ee2f78027a2ffd20a5824d933a01b48fcf4854a8
Eyad Ahmed <eyad.ahmed.abdelfatah@gmail.com>
2025-04-10 14:18:53 +0200

    protected profile endpoint

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const getemailfromtoken = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}/profile`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      );
+      if (data) {
+        setEmail(data.user.email);
+      }
+    } catch (err: any) {
+      console.error("Error token in:", err);
+    }
+  };
+  useEffect(() => {
+    getemailfromtoken();
+  }, []);
   return (
-    <div style={{ alignItems: "center" }}>
+    <div>
       <h1>Welcome to the application.</h1>
+      <p>Your email is: {email}</p>
     </div>
   );
 };
